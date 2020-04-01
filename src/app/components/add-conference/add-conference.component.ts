5f3826768b0b5fbdd96b03ae59fe5e47493d9bcb
Dariusz Dudek <darekdede1995@gmail.com>
2020-04-01 17:01:03 +0200

    finished new conference form

--- src/app/components/add-conference/add-conference.component.ts
+++ src/app/components/add-conference/add-conference.component.ts
@@ -18,89 +18,96 @@ export class AddConferenceComponent implements OnInit {
     ) {}
 
     ngOnInit() {
-      this.form = new FormGroup({
-          name: new FormControl("Conference name", [Validators.required]),
-          country: new FormControl("Poland", [Validators.required]),
-          startDate: new FormControl(new Date(), [Validators.required]),
-          endDate: new FormControl(new Date(), [Validators.required]),
-          price: new FormControl(1200, [Validators.required]),
-          freePlaces: new FormControl(20, [Validators.required]),
-          description: new FormControl("Conference description", [
-              Validators.required
-          ]),
-          thumbnailUrl: new FormControl(
-              "https://cdn.pixabay.com/photo/2016/10/17/10/52/wind-farm-1747331_960_720.jpg",
-              [Validators.required]
-          )
-      });
-  }
+        this.form = new FormGroup({
+            name: new FormControl("", [Validators.required]),
+            city: new FormControl("", [Validators.required]),
+            organizer: new FormControl("", [Validators.required]),
+            contact: new FormControl("", [Validators.required]),
+            category: new FormControl("", [Validators.required]),
+            startDate: new FormControl(new Date(), [Validators.required]),
+            endDate: new FormControl(new Date(), [Validators.required]),
+            price: new FormControl("", [Validators.required]),
+            slots: new FormControl("", [Validators.required]),
+            detailUrl: new FormControl("", [Validators.required])
+        });
+    }
 
-  get name() {
-      return this.form.get("name");
-  }
+    get name() {
+        return this.form.get("name");
+    }
 
-  get country() {
-      return this.form.get("country");
-  }
+    get city() {
+        return this.form.get("city");
+    }
 
-  get startDate() {
-      return this.form.get("startDate");
-  }
+    get organizer() {
+        return this.form.get("organizer");
+    }
+    get contact() {
+        return this.form.get("contact");
+    }
 
-  get endDate() {
-      return this.form.get("endDate");
-  }
+    get category() {
+        return this.form.get("category");
+    }
 
-  get price() {
-      return this.form.get("price");
-  }
+    get startDate() {
+        return this.form.get("startDate");
+    }
 
-  get freePlaces() {
-      return this.form.get("freePlaces");
-  }
+    get endDate() {
+        return this.form.get("endDate");
+    }
 
-  get description() {
-      return this.form.get("description");
-  }
+    get price() {
+        return this.form.get("price");
+    }
 
-  get thumbnailUrl() {
-      return this.form.get("thumbnailUrl");
-  }
+    get slots() {
+        return this.form.get("slots");
+    }
 
-  async createConference() {
-      const {
-          name,
-          country,
-          startDate,
-          endDate,
-          price,
-          freePlaces,
-          description,
-          thumbnailUrl
-      } = this.form.value;
+    get description() {
+        return this.form.get("description");
+    }
 
-      const newConference: NewConference = {
-          name,
-          country,
-          startDate: new Date(startDate).toISOString(),
-          endDate: new Date(endDate).toISOString(),
-          price,
-          freePlaces,
-          description,
-          thumbnailUrl,
-          rating: {
-              value: 0,
-              votes: []
-          },
-          comments: []
-      };
+    get detailUrl() {
+        return this.form.get("detailUrl");
+    }
 
-      console.log("createConference", {
-          form: this.form.value,
-          newConference
-      });
-      await this.conferencesService.addConference(newConference);
-      this.router.navigate(["/conferences"]);
-  }
+    async createConference() {
+        const {
+            name,
+            city,
+            organizer,
+            contact,
+            category,
+            startDate,
+            endDate,
+            price,
+            slots,
+            detailUrl
+        } = this.form.value;
+
+        const newConference: NewConference = {
+            name,
+            city,
+            organizer,
+            contact,
+            category,
+            startDate: new Date(startDate).toISOString(),
+            endDate: new Date(endDate).toISOString(),
+            price,
+            slots,
+            detailUrl,
+            comments: []
+        };
+
+        console.log("createConference", {
+            form: this.form.value,
+            newConference
+        });
+        await this.conferencesService.addConference(newConference);
+        this.router.navigate(["/conferences"]);
+    }
 }
- 
--- src/app/models/Conference.ts
+++ src/app/models/Conference.ts
@@ -1,48 +1,44 @@
 export interface ConferenceComment {
-
     timestamp: string;
 
     author: string;
 
     content: string;
-
 }
 
 export interface ConferenceRating {
-
     value: number;
 
     votes: string[];
-
 }
 
 export interface Conference {
-
     key: string;
 
     name: string;
 
-    country: string;
+    city: string; //country
 
     startDate: string;
+
+    organizer: string; //new
+
+    category: string; //new
 
     endDate: string;
 
     price: number;
 
-    freePlaces: number;
+    slots: number;
 
-    description: string;
+    detailUrl: string;
 
-    thumbnailUrl: string;
-
-    rating: ConferenceRating;
+    contact: string;
 
     comments?: ConferenceComment[];
-
 }
 
-export type NewConference = Omit<Conference, 'key'>;
+export type NewConference = Omit<Conference, "key">;
 
 export interface SelectedConference extends Conference {
     selectedPlacesCount: number;
