fccc474ad6ae241f329aa18030602c3860160e5c
Dariusz Dudek <darekdede1995@gmail.com>
2020-04-01 19:09:17 +0200

    added conference thumbnail

--- src/app/components/conference-thumbnail/conference-thumbnail.component.ts
+++ src/app/components/conference-thumbnail/conference-thumbnail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Conference } from "src/app/models/Conference";
+import { BookingService } from '../../services/booking/booking.service';
+import { BookingList } from '../../models/BookingList';
 
 @Component({
     selector: "app-conference-thumbnail",
@@ -9,7 +11,42 @@ import { Conference } from "src/app/models/Conference";
 export class ConferenceThumbnailComponent implements OnInit {
     @Input() conference: Conference;
 
-    constructor() {}
+    constructor(private bookingService: BookingService) {}
 
     ngOnInit() {}
+
+    preventNavigation($event) {
+        $event.stopPropagation();
+        $event.preventDefault();
+    }
+
+    getReservedPlacesCount(conference: Conference): number {
+        return (
+            this.bookingService.bookingList$.value.conferences.get(conference.key) || 0
+        );
+    }
+
+    handleAdd(conference: Conference) {
+        const bookingList: BookingList = this.bookingService.bookingList$
+            .value;
+        const currentCount = this.getReservedPlacesCount(conference);
+        if (currentCount + 1 <= conference.slots) {
+            bookingList.conferences.set(conference.key, currentCount + 1);
+            this.bookingService.bookingList$.next(bookingList);
+        }
+    }
+
+    handleSubtract(conference: Conference) {
+        const bookingList: BookingList = this.bookingService.bookingList$
+            .value;
+        const currentCount = this.getReservedPlacesCount(conference);
+        if (currentCount - 1 > 0) {
+            bookingList.conferences.set(conference.key, currentCount - 1);
+            this.bookingService.bookingList$.next(bookingList);
+        }
+        if (currentCount - 1 === 0) {
+            bookingList.conferences.delete(conference.key);
+            this.bookingService.bookingList$.next(bookingList);
+        }
+    }
 }
