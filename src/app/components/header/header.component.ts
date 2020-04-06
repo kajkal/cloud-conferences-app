e242eb315b1a8211406faae8e7b06fd461dda85a
kajkal <karolesniak@gmail.com>
2020-04-06 15:29:41 +0200

    Revert "enabled admin mode" This reverts commit ca05a281

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,32 +1,38 @@
-import { Component } from "@angular/core";
-import { AuthService } from "../../services/auth/auth.service";
-import { interval, Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { interval, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 enum HeaderMode {
-    GUEST = "GUEST", // not authorized user
-    USER = "USER", // authorized user
-    ADMIN = "ADMIN" // authorized user with admin role
+    GUEST = 'GUEST', // not authorized user
+    USER = 'USER', // authorized user
+    ADMIN = 'ADMIN', // authorized user with admin role
 }
 
 @Component({
-    selector: "app-header",
-    templateUrl: "./header.component.html",
-    styleUrls: ["./header.component.scss"]
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: [ './header.component.scss' ],
 })
 export class HeaderComponent {
+
     mode$: Observable<HeaderMode>;
     selectedPlacesCount$: Observable<number>;
 
-    constructor(private authService: AuthService) {
-        this.mode$ = authService.authState$.pipe(
-            map(authState => {
-                if (authState === null) {
-                    return HeaderMode.GUEST;
-                }
-                return authState.isAdmin ? HeaderMode.ADMIN : HeaderMode.USER;
-            })
-        );
+    constructor(
+        private authService: AuthService,
+    ) {
+        this.mode$ = authService.authState$
+            .pipe(
+                map((authState) => {
+                    if (authState === null) {
+                        return HeaderMode.GUEST;
+                    }
+                    return (authState.isAdmin) ? HeaderMode.ADMIN : HeaderMode.USER;
+                }),
+            );
         this.selectedPlacesCount$ = interval(1000); // todo connect with cart service
     }
+
 }
